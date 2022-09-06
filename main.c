2d6d040195ebe21c248dd6a040a944d31c5d210c
Julien Caucheteux <jcauchet@c2r11p3.42nice.fr>
2022-09-06 11:48:45 +0200

    Added basic textures & small edits

--- main.c
+++ main.c
@@ -6,16 +6,34 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 14:19:08 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/09/06 10:53:49 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/09/06 11:30:13 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3d.h"
 
+int	syntax_error(int ac, char **av)
+{
+	int	i;
+	
+	if (ac != 2)
+		return (1);
+	i = 0;
+	while (av[1][i])
+		i++;
+	i--;
+	if (ft_strlen(av[1]) < 4)
+		return (1);
+	// jcauchetcomment: check here if there's .cub at the end of the file's name.
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
-	printf("salut\n");
-	printf("test\n");
+	if (syntax_error(ac, av))
+	{
+		printf("Error\nPlease use a correct format: ./cub3d map_name.cub\n");
+		return (1);
+	}
+	
 }
