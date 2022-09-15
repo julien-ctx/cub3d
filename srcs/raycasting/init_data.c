8c9d6ac5f9b2223b86ad58b6fb7f95ee138f0cc8
Julien Caucheteux <jcauchet@c2r11p3.42nice.fr>
2022-09-15 15:54:25 +0200

    Fixed orientation bug

--- srcs/raycasting/init_data.c
+++ srcs/raycasting/init_data.c
@@ -6,7 +6,7 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:37:39 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/09/14 23:25:22 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/09/15 15:52:27 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,19 @@ void	set_plane(t_d *data, char c)
 {
 	if (c == 'N' || c == 'S')
 	{
-		data->plane_x = 0.66;
+		if (c == 'N')
+			data->plane_x = 0.66;
+		else
+			data->plane_x = -0.66;
 		data->plane_y = 0;
 	}
 	else
 	{
 		data->plane_x = 0;
-		data->plane_y = -0.66;
+		if (c == 'E')
+			data->plane_y = 0.66;
+		else
+			data->plane_y = -0.66;
 	}
 }
 
@@ -64,8 +70,8 @@ void	player_pos_init(t_d *data, char **tab)
 			if (tab[i][j] == 'N' || tab[i][j] == 'S' || tab[i][j] == 'E'
 				|| tab[i][j] == 'W')
 			{
-				data->pos_x = j;
-				data->pos_y = i;
+				data->pos_x = j + 0.5;
+				data->pos_y = i + 0.5;
 				set_orientation(data, tab[i][j]);
 				set_plane(data, tab[i][j]);
 			}
